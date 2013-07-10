4beda7dd713297e9e26cfec917b51ba961f7ec9f
oofaish <oofaish@cigar.local>
2013-07-10 07:52:35 +0100

    Check if 3d is supported and warn if not

--- countdowncube.js
+++ countdowncube.js
@@ -11,83 +11,118 @@
 ;(function ( $, window, document, undefined ) {
 
     /**********************************************************
-	** helpers
-	** basically where all the function are defined
+    ** helpers
+    **
+    ** basically where all the function are defined
 	***********************************************************/
     var helpers = { 
     		
+            has3d: function()
+            {
+                        var el = document.createElement('p'),
+            
+                        transforms = {
+                            'webkitTransform':'-webkit-transform',
+                            'OTransform':'-o-transform',
+                            'msTransform':'-ms-transform',
+                            'MozTransform':'-moz-transform',
+                            'transform':'transform'
+                        };
+                    
+                        // Add it to the body to get the computed style
+                        document.body.insertBefore(el, null);
+                    
+                        for(var t in transforms){
+                            if( el.style[t] !== undefined ){
+                                el.style[t] = 'translate3d(1px,1px,1px)';
+                                has3d = window.getComputedStyle(el).getPropertyValue(transforms[t]);
+                            }
+                        }
+                    
+                        document.body.removeChild(el);
+                    
+                        return (has3d !== undefined && has3d.length > 0 && has3d !== "none");
+            },
+                
     	    /**********************************************************
-    		** init
-    		** The main function - actually resets everything each time
-    		** you call it
-    		***********************************************************/
-    		init: function( element, options ) {
-            	//remove any existing elements
-    			element.find('*').remove();
+            ** init
+            ** The main function - actually resets everything each time
+            ** you call it
+            ***********************************************************/
+            init: function( element, options ) {
+            //remove any existing elements
+                    element.find('*').remove();
+            
+                    //if a cube is already defined, remove it and clear the timer
+            if( typeof element.data('countdownCubeId' ) != 'undefined' )
+            {
+                        clearInterval( element.data('countdownCubeId' ) );
+                        element.removeData('countdownCubeId');
+            };
+                        
+            //somehow keep track of all the various countdowns
+            if( typeof $('body').data('countdownCubeCounter') == 'undefined' )
+                    $('body').data( 'countdownCubeCounter', 0 );
+            else
+                    $('body').data( 'countdownCubeCounter', $('body').data( 'countdownCubeCounter' ) + 1 );
+            
+            this.elementClass = 'countdownCube_ID_' +  $('body').data( 'countdownCubeCounter' );
+            
+            element.addClass( this.elementClass );
+                            
+            this.classes = [ 'front','back', 'right', 'left', 'top', 'bottom' ];	
+                    this.topTags = [ 'year', 'month', 'day','hour', 'minute', 'second' ];
+                    this.loadingTags = [ 'LO', 'AD', 'IN', 'G.', '..', '...'];
+                    this.transformNames =  [ '-webkit-transform', '-moz-transform', '-o-transform', 'transform' ];
+                    
+                    cubes = [];
+                    
+                    //add the figures, etc to the div
+                    
+                    for( var tagIndex in this.topTags )
+                    {
+                            var tag = this.topTags[ tagIndex ];
+                            
+                            cube = element.append('<section></section>')
+                                       .children(':last')	   
+                                            .attr('id', tag )
+                                       .addClass( "countdownCubeContainer" )
+                                       .append('<div></div>')
+                                       .children(':last')
+                                       .addClass('countdownCubeCube')
+                                       .data('side', 'show-front');
+                            /*this is horrible, chaining would be much cooler*/
+                            this.addFigures( cube, this.classes, this.loadingTags[ tagIndex ] );
+                            
+                            element.children(':last').append('<div></div>').children(':last').html(tag).addClass('countdownCubeTitleDiv');
+                    }
+                     
+                    this.setupAllCss( element, options );
 
-    			//if a cube is already defined, remove it and clear the timer
-    			if( typeof element.data('countdownCubeId' ) != 'undefined' )
-            	{
-            		clearInterval( element.data('countdownCubeId' ) );
-            		element.removeData('countdownCubeId');
-            	};
-            	
-            	//somehow keep track of all the various countdowns
-            	if( typeof $('body').data('countdownCubeCounter') == 'undefined' )
-            		$('body').data( 'countdownCubeCounter', 0 );
-            	else
-            		$('body').data( 'countdownCubeCounter', $('body').data( 'countdownCubeCounter' ) + 1 );
-            	
-            	this.elementClass = 'countdownCube_ID_' +  $('body').data( 'countdownCubeCounter' );
-            	
-            	element.addClass( this.elementClass );
-            	            	
-            	this.classes = [ 'front','back', 'right', 'left', 'top', 'bottom' ];	
-        		this.topTags = [ 'year', 'month', 'day','hour', 'minute', 'second' ];
-        		this.loadingTags = [ 'LO', 'AD', 'IN', 'G.', '..', '...'];
-        		this.transformNames =  [ '-webkit-transform', '-moz-transform', '-o-transform', 'transform' ];
-        		
-        		cubes = [];
-        		
-        		//add the figures, etc to the div
-        		
-        		for( var tagIndex in this.topTags )
-        		{
-        			var tag = this.topTags[ tagIndex ];
-        			
-        			cube = element.append('<section></section>')
-        				   .children(':last')	   
-        					.attr('id', tag )
-        				   .addClass( "countdownCubeContainer" )
-        				   .append('<div></div>')
-        				   .children(':last')
-        				   .addClass('countdownCubeCube')
-        				   .data('side', 'show-front');
-        			/*this is horrible, chaining would be much cooler*/
-        			this.addFigures( cube, this.classes, this.loadingTags[ tagIndex ] );
-        			
-        			element.children(':last').append('<div></div>').children(':last').html(tag).addClass('countdownCubeTitleDiv');
-        		}
-        		 
-        		this.setupAllCss( element, options );
+                    /*actually use this offset so you handle people in different time zones*/
+                    /*var offset = options.target.getTimezoneOffset() + 60;//in minutes;*/
+                    
+                    var that = this;
+                    /*set it up for the first time, and then set a timer, to refresh every second*/
+                    
+                setTimeout( function(){ that.setTimeLeft( element, options );}, 10 );
+                var refreshId = setInterval( function(){ that.setTimeLeft( element, options );}, 1000 );
+                element.data('countdownCubeId', refreshId );
+                
+                        if ( !this.has3d() )
+                        {
+                                    element.append("<p>Your Browser/Computer Sucks - It doesnt support 3d Transforms!</p>");
+                        }           
 
-        		/*actually use this offset so you handle people in different time zones*/
-        		/*var offset = options.target.getTimezoneOffset() + 60;//in minutes;*/
-        		
-        		var that = this;
-        		/*set it up for the first time, and then set a timer, to refresh every second*/
-        		
-        	    setTimeout( function(){ that.setTimeLeft( element, options );}, 10 );
-        	    var refreshId = setInterval( function(){ that.setTimeLeft( element, options );}, 1000 );
-        	    element.data('countdownCubeId', refreshId );
+                
             },
 
             /**********************************************************
-    		** setupAllCss
-    		** set all the various CSS properties that I need
-    		** experimenting a little with various different
-    		** ways of setting css properties actually.
-    		***********************************************************/
+            ** setupAllCss
+            ** set all the various CSS properties that I need
+            ** experimenting a little with various different
+            ** ways of setting css properties actually.
+            ***********************************************************/
             setupAllCss: function( element, options )
             {
             	element.css({
@@ -147,10 +182,10 @@
             },
             
     	    /**********************************************************
-    		** createStyleSheet
-    		** create the innerHTML of a stylesheet for the cube
-    		** transforms
-    		***********************************************************/
+            ** createStyleSheet
+            ** create the innerHTML of a stylesheet for the cube
+            ** transforms
+            ***********************************************************/
             createStyleSheet: function( element, childSelector, transform )
             {
             	var newCssProperties = '{';
@@ -164,9 +199,9 @@
             },
             
     	    /**********************************************************
-    		** addStyleSheet
-    		** Add the stylesheet to the head
-    		***********************************************************/
+            ** addStyleSheet
+            ** Add the stylesheet to the head
+            ***********************************************************/
             addStyleSheet: function( styleSheet )
             {
             	var style = document.createElement('style');
@@ -175,10 +210,10 @@
             	$( style ).appendTo('head');
             },
     	    /**********************************************************
-    		** addTransforms
-    		** a different way of adding CSS properties for the 
-    		** cube
-    		***********************************************************/
+            ** addTransforms
+            ** a different way of adding CSS properties for the 
+            ** cube
+            ***********************************************************/
 
             addTransforms: function( element, childSelector, transform )
             {
@@ -190,9 +225,9 @@
             },
 
             /**********************************************************
-    		** addFigures
-    		** add the figures to each cube
-    		***********************************************************/
+            ** addFigures
+            ** add the figures to each cube
+            ***********************************************************/
             addFigures: function( cube, classes, loadingTag )
             {
             	var i;
